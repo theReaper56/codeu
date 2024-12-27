03387cc1581ae11934aadea74057b27d59c205aa
theReaper56 <harshitupadhyay083@gmail.com>
2024-12-27 19:53:36 +0530

    new file:   he.html 	new file:   he.js     hello

---
+++ he.js
@@ -0,0 +1,14 @@
+ul=document.createElement("ul");
+document.body.appendChild(ul);
+while(true)
+{
+    data=prompt("Enter");
+    if(data==null||data=="")
+        break;
+
+    li=document.createElement("li");
+    li.innerHTML=data;
+    ul.appendChild(li);
+    alert(""+li.innerHTML+"");
+    
+}
